c2f32b772cf1912aeaa27394c338c30b33422940
gustavolimadesousa <gustavolimadesousa@hotmail.com>
2025-01-25 22:09:42 +0000

    group assignment week 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParams("product");
 
-function addProductToCart(product) {
-  const currentCart = getLocalStorage("so-cart") || []; 
-  currentCart.push(product);
-  setLocalStorage("so-cart", currentCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
