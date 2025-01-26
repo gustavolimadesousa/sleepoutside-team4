5815189f82bfb36ff60a980878a1e65bb12b1abd
evenage <charue.vyago2019@gmail.com>
2025-01-26 14:36:23 +0200

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
 
-loadHeaderFooter();
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const listing = new ProductListing("Tents", dataSource, element);
+
+listing.init();
+
